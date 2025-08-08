67fd9d30eaf68dc5791cd89340ac13f1fbc78379
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-08-08 17:11:01 +0200

    Improve logic for layer coloring

--- keyboards/keychron/q3/ansi/keymaps/wmeints/keymap.c
+++ keyboards/keychron/q3/ansi/keymaps/wmeints/keymap.c
@@ -21,10 +21,14 @@
 
 enum {
     TD_C,
+    TD_X,
+    TD_V,
 };
 
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_C] = ACTION_TAP_DANCE_DOUBLE(KC_C, LCTL(KC_C))
+    [TD_C] = ACTION_TAP_DANCE_DOUBLE(KC_C, LCTL(KC_C)),
+    [TD_X] = ACTION_TAP_DANCE_DOUBLE(KC_X, LCTL(KC_X)),
+    [TD_V] = ACTION_TAP_DANCE_DOUBLE(KC_V, LCTL(KC_V)),
 };
 
 enum layers {
@@ -57,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,
-        KC_LSFT,            KC_Z,     KC_X,     TD(TD_C), KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,
+        KC_LSFT,            KC_Z,     TD(TD_X), TD(TD_C), TD(TD_V), KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_tkl_ansi(
@@ -76,6 +80,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,  _______,  _______,  _______),
 };
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch(get_highest_layer(state)) {
+        case MAC_BASE:
+            rgb_matrix_mode(RGB_MATRIX_SPLASH);
+            rgb_matrix_sethsv(0, 0, 255); // White splash on black
+            break;
+        case MAC_FN:
+            rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
+            rgb_matrix_sethsv(HSV_GREEN); // Green for function
+            break;
+        case WIN_BASE:
+            rgb_matrix_mode(RGB_MATRIX_SPLASH);
+            rgb_matrix_sethsv(0, 0, 255); // White splash on black
+            break;
+        case WIN_FN:
+            rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
+            rgb_matrix_sethsv(HSV_GREEN); // Green for function
+            break;
+        case WIN_VSC:
+            rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
+            rgb_matrix_sethsv(HSV_RED); // Green for Visual Studio Code
+            break;
+    }
+
+    return state;
+}
 
 void keyboard_post_init_user(void) {
     rgb_matrix_mode(RGB_MATRIX_SPLASH);
